377360edff9b3f82ae83beb1f036d558f6d5e774
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2022-11-16 15:48:48 -0300

    update folder

--- script/061chm2tree.R
+++ script/061chm2tree.R
@@ -10,8 +10,11 @@ require(tools)
 chm.files = list.files("./chm/")
 chm.files
 
-ctg = raster(paste0("./chm/", chm.files[1]))
-trees = locate_trees(ctg, lmf(ws = 30))
+chm.files = raster(paste0("./chm/", chm.files[1]))
+trees = locate_trees(chm.files, lmf(ws = 30))
 
 trees = as(trees, 'Spatial')
-writeOGR(trees, dsn = "./chm/", layer = file_path_sans_ext(chm.files[1]) , driver = "ESRI Shapefile")
+trees70 = subset(trees, Z > 70)
+
+#writeOGR(trees, dsn = "./trees/", layer = file_path_sans_ext(chm.files[1]), driver = "ESRI Shapefile")
+writeOGR(trees, "./trees/", file_path_sans_ext(chm.files[1]), driver="ESRI Shapefile")
